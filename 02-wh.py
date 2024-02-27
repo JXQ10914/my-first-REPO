645a9d3d7a32c722604ec7163f98bf075b30eac7
wanghao <1415474337@qq.com>
2024-02-27 11:06:21 +0800

    feat: 添加数据类型示例

---
+++ 02-wh.py
@@ -0,0 +1,7 @@
+number = 1 #数字
+string = 'string' #字符串
+boolean = True #布尔
+list = [1,'string',False] #列表（我之前一直说的数组是JavaScript里的概念）
+tuple = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'] #元组（长得跟列表很像，但是列表长度可变，元组定了就不能变了）
+set = {'jxq','beauty'} #集合
+dictionary = {'jxq':'beauty'} #字典
